fc35f4a5c299ddbfac47a0db7cc881f2597d328f
bappi12312 <ranamdbappihossain@gmail.com>
2025-02-20 23:22:50 +0600

    some proBlema solvEd

--- src/app.js
+++ src/app.js
@@ -4,27 +4,28 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send('Hello, Express!');
 })
 
 app.use(
   cors({
-    origin: ['http://localhost:3000',"https://tto-platform-blush.vercel.app"], // Allow specific frontend origin
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    origin: ['http://localhost:3000', "https://tto-platform-blush.vercel.app"], // Allow specific frontend origin
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow credentials
   })
 );
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.urlencoded({extended: true,limit: '16kb'}))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.static('public'))
 app.use((err, req, res, next) => {
   const statusCode = err.status || 500;
   res.status(statusCode).json({
-      message: err.message || "Internal Server Error",
-      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 });
 
@@ -32,7 +33,7 @@ app.use((err, req, res, next) => {
 // router import
 import userRouter from "./routes/user.routes.js"
 
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users", userRouter)
 
 export {
   app
