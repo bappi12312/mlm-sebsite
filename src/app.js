092489ff5054a63857fd222bfd08fe04c893003f
bappi12312 <ranamdbappihossain@gmail.com>
2025-02-21 00:36:14 +0600

    what the problem

--- src/app.js
+++ src/app.js
@@ -17,9 +17,10 @@ app.use(
   })
 );
 
-app.use(express.json())
+
 app.use(cookieParser())
-app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.static('public'))
 app.use((err, req, res, next) => {
   const statusCode = err.status || 500;
